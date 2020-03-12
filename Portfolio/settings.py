adfaaa2c9172ff5cf68210fe2865840403b51fbe
WebDev679 <arnavpchaudhry3@gmail.com>
2020-03-12 20:41:13 +0530

    Projects App is made and postgres is up and running

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'projects.apps.ProjectsConfig'
+    'projects.apps.ProjectsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'Portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD':'25091813',
+        'HOST':'localhost',
+        'PORT':'5432',
+
     }
 }
 
